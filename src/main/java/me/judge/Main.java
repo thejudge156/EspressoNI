30f2ec16e31cd40631798ff82a129e0ebee78f62
The Judge <53906078+thejudge156@users.noreply.github.com>
2024-12-11 09:18:49 -0800

    Try using the superclass

--- src/main/java/me/judge/Main.java
+++ src/main/java/me/judge/Main.java
@@ -8,12 +8,14 @@ import java.io.File;
 public class Main {
     public static void main(String[] args) {
         File testJar = new File("testjar.jar");
+        File apiJar = new File("api.jar");
         Context ctx = Context.newBuilder("java")
-                .option("java.Properties.java.class.path", testJar.getAbsolutePath() + ":" + System.getProperty("java.class.path"))
+                .option("java.Properties.java.class.path", testJar.getAbsolutePath() + ":" + apiJar.getAbsolutePath())
                 .allowAllAccess(true)
                 .build();
         Value pluginClazz = ctx.getBindings("java").getMember("java.lang.Class").invokeMember("forName", "me.judge.Usage");
-        JavaPlugin plugin = pluginClazz.as(JavaPlugin.class);
+        JavaPlugin plugin = pluginClazz.invokeMember("getDeclaredConstructor").invokeMember("newInstance")
+                .invokeMember("super").as(JavaPlugin.class);
         plugin.onEnable();
 
         ctx.close();
