075e635191ba68db87967fcf6fa2353014b81d0d
The Judge <53906078+thejudge156@users.noreply.github.com>
2024-12-10 17:16:49 -0800

    Try a slightly different approach 2

--- src/main/java/me/judge/Main.java
+++ src/main/java/me/judge/Main.java
@@ -1,28 +1,27 @@
 package me.judge;
 
 import org.graalvm.polyglot.Context;
-import org.graalvm.polyglot.Engine;
 import org.graalvm.polyglot.Value;
 
 import java.io.File;
+import java.io.IOException;
+import java.net.URL;
 
 public class Main {
     public static void main(String[] args) {
         File testJar = new File("testjar.jar");
-        Engine engine = Engine.newBuilder()
-                .allowExperimentalOptions(true)
-                .option("compiler.Inlining", "false")
-                .option("engine.WarnInterpreterOnly", "false")
-                .build();
         try(Context ctx = Context.newBuilder("java")
                 .allowAllAccess(true)
-                .option("java.Properties.java.class.path", testJar.getAbsolutePath())
-                .engine(engine)
                 .build()) {
-            ctx.getBindings("java").putMember("me.judge.JavaPlugin", JavaPlugin.class);
-            Value pluginInstance = ctx.getBindings("java").getMember("me.judge.Usage").invokeMember("getDeclaredConstructor").invokeMember("newInstance");
+            Value loaderClazzVal = ctx.getBindings("java").getMember("java.net.URLClassLoader");
+            Value loaderMethodVal = loaderClazzVal.invokeMember("getMethod", "newInstance", ctx.asValue(URL[].class), ctx.asValue(ClassLoader.class));
+            Value loaderVal = loaderMethodVal.invokeMember("invoke", ctx.asValue(testJar.toURI().toURL()), ctx.asValue(ctx.getClass().getClassLoader()));
+            Value pluginClazz = loaderVal.invokeMember("loadClass", "me.judge.Usage");
+            Value pluginInstance = pluginClazz.invokeMember("getDeclaredConstructor").invokeMember("newInstance");
             JavaPlugin plugin = pluginInstance.as(JavaPlugin.class);
             plugin.onEnable();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 
