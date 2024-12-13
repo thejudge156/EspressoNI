4067734adbea99d739cec863fba48f86327e845b
The Judge <53906078+thejudge156@users.noreply.github.com>
2024-12-13 15:52:44 -0800

    hm.. pt 2

--- src/main/java/me/judge/Main.java
+++ src/main/java/me/judge/Main.java
@@ -8,12 +8,11 @@ import java.io.File;
 public class Main {
     public static void main(String[] args) {
         File testJar = new File("testjar.jar");
-        File apiJar = new File("api.jar");
         Context ctx = Context.newBuilder("java")
-                .option("java.Classpath", testJar.getAbsolutePath() + ":" + apiJar.getAbsolutePath())
+                .option("java.Classpath", testJar.getAbsolutePath())
                 .allowAllAccess(true)
                 .build();
-        Value pluginClazz = ctx.getBindings("java").getMember("me.judge.Usage");
+        Value pluginClazz = ctx.getBindings("java.lang.Class").invokeMember("forName", "me.judge.Usage", false, ClassLoader.getPlatformClassLoader());
         System.out.println(pluginClazz.getMemberKeys());
 
         ctx.close(true);
