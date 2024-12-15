019e61f0b973862324f0dac890577654ce73c8fd
The Judge <colepatirckroberts@gmail.com>
2024-12-14 19:16:09 -0800

    Check

--- src/main/java/me/judge/Main.java
+++ src/main/java/me/judge/Main.java
@@ -1,23 +1,43 @@
 package me.judge;
 
+import org.apache.fury.Fury;
+import org.apache.fury.config.Language;
 import org.graalvm.polyglot.Context;
 import org.graalvm.polyglot.Value;
 
 import java.io.File;
+import java.lang.reflect.InvocationTargetException;
 
 public class Main {
-    public static void main(String[] args) {
+    static Fury fury;
+
+    static {
+        fury = Fury.builder()
+                .requireClassRegistration(false)
+                .withLanguage(Language.JAVA)
+                .build();
+    }
+
+    public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException, ClassNotFoundException {
         File testJar = new File("testjar.jar");
         Context ctx = Context.newBuilder("java")
-                .option("java.Classpath", testJar.getAbsolutePath())
-                .option("engine.RelaxStaticObjectSafetyChecks", "true")
+                .option("java.Classpath", testJar.getAbsolutePath() + ";" + System.getProperty("java.class.path"))
                 .allowAllAccess(true)
                 .build();
-        ctx.getBindings("java").getMember("java.lang.Class").invokeMember("forName", "me.judge.Usage", true, Main.class.getClassLoader());
-        Value pluginClazz = ctx.getBindings("java").getMember("java.lang.Class").invokeMember("forName", "me.judge.Usage", false, ctx.asValue(ClassLoader.getPlatformClassLoader()));
-        System.out.println(pluginClazz.getMemberKeys());
+        Value furyVal = ctx.getBindings("java").getMember("org.apache.fury.Fury");
+        Value furyInstVal = furyVal.invokeMember("builder")
+                .invokeMember("withLanguage", ctx.getBindings("java")
+                        .getMember("org.apache.fury.config.Language")
+                        .getMember("JAVA"))
+                .invokeMember("build");
+        Value clazz = ctx.getBindings("java").getMember("me.judge.Usage");
+        Value byteVal = furyInstVal.invokeMember("serialize", clazz.getMember("class"));
+        Class<?> pluginClazz = (Class<?>) fury.deserialize(byteVal.as(byte[].class));
 
-        ctx.close(true);
+        JavaPlugin plugin = (JavaPlugin) pluginClazz.getDeclaredConstructor().newInstance();
+        plugin.onEnable();
+
+        ctx.close();
     }
 
     public interface JavaPlugin {
